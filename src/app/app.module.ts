c2e262080e21cf168387d2cba05f86f5d7e6af7d
Chris Hodges <chris.hodges@gmail.com>
2018-11-12 21:44:13 -0600

    Cleanup unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MainComponent } from './main/main.component';
 import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -11,7 +10,6 @@ import { LayoutModule } from '@angular/cdk/layout';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     MainComponent
   ],
   imports: [
