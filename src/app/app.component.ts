920c2232f8a4f83e0e788a1df36a733539b5614a
Chris Hodges <chris.hodges@adtran.com>
2018-12-23 19:41:10 -0600

    Change name again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, NgModule } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DevSwing';
+  title = 'LeanAgileFlowDashboard';
 }
