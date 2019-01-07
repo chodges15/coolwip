e531b270f4d83f8e0ae641fa23318f3a70f7ddcd
Chris Hodges <chris.hodges@gmail.com>
2019-01-06 23:30:04 -0600

    Start working towards a simple octokit/rest mock

--- src/app/github.service.spec.ts
+++ src/app/github.service.spec.ts
@@ -1,12 +1,48 @@
 import { TestBed } from '@angular/core/testing';
-
-import { GithubService } from './github.service';
+import * as Octokit from '@octokit/rest';
+import { GithubService, IssueSearchResult } from './github.service';
+import { tokenKey } from '@angular/core/src/view';
+import { stringify } from '@angular/core/src/render3/util';
+import * as mockIssueJsonResponse from './github-issue-search-result.json';
 
 describe('GithubService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let githubService: GithubService;
+  let octokitSpy: jasmine.SpyObj<Octokit>;
+  beforeEach(() => {
+    const spy = jasmine.createSpyObj('octokit',
+    {
+      'authenticate' : function(any) { },
+      'search' : {
+        'issues' : function(any) { return new Promise(res => setTimeout(res, 0)); }
+      }
+    });
+
+    console.log(spy);
+    TestBed.configureTestingModule({
+      providers: [
+        GithubService,
+        { provide: Octokit, use: spy },
+        { provide: 'githubToken', useValue: '06b44a41abf1fce37f2aee6570bb46b46c05bdf8' },
+      ]
+    });
+    githubService = TestBed.get(GithubService);
+    octokitSpy = TestBed.get(Octokit);
+  });
 
   it('should be created', () => {
-    const service: GithubService = TestBed.get(GithubService);
-    expect(service).toBeTruthy();
+    expect(githubService).toBeTruthy();
+  });
+
+  describe('getTeamPullRequests()', () => {
+
+    it('should return an Observable<Array<PullRequest>>', () => {
+      const service: GithubService = TestBed.get(GithubService);
+      service.getTeamPullRequests('LeanAgileFlowSoftware', 'test').subscribe((pullRequests) => {
+        const response: Octokit.Response<IssueSearchResult> = pullRequests;
+        expect(response.data.items.length).toBe(2);
+        expect(response.data.items[0].id).toEqual('1');
+        expect(response.data.items[1].id).toEqual('2');
+      });
+    });
   });
 });
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { PullRequest } from './pull-request';
-import { MOCK_PULL_REQUESTS } from './mock-pull-requests';
-import { Observable, of } from 'rxjs';
+import { MOCK_PULL_REQUESTS } from './mock-github-service-data';
+import { Observable, of, from } from 'rxjs';
 import * as Octokit from '@octokit/rest';
 
 @Injectable({
@@ -9,10 +9,79 @@ import * as Octokit from '@octokit/rest';
 })
 export class GithubService {
 
-  constructor() { }
 
-  getPullRequests(organization: String, user: String): Observable<PullRequest[]> {
-    console.log('Fetching pull requests');
-    return of(MOCK_PULL_REQUESTS);
+  readonly queryBasePr: string = `type:pr`;
+  constructor(@Inject('octokit') public octokit: Octokit, @Inject('githubToken') public githubToken: string) {
+    console.log(octokit);
+    const authParams: Octokit.AuthUserToken = { type: 'token', token: githubToken };
+    this.octokit.authenticate(authParams);
   }
+
+  getTeamPullRequests(organization: string, teamName: string): Observable<Octokit.Response<IssueSearchResult>> {
+    const query = `{ queryBasePr }+team:{ organization }/{ teamName }`;
+    const searchIssues: Octokit.SearchIssuesParams = { q: query };
+    return from(this.octokit.search.issues(searchIssues));
+  }
+}
+
+export interface User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+export interface PullRequest {
+  url: string;
+  html_url: string;
+  diff_url: string;
+  patch_url: string;
+}
+
+export interface Issue {
+  url: string;
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+  html_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  user: User;
+  labels: any[];
+  state: string;
+  locked: boolean;
+  assignee?: any;
+  assignees: any[];
+  milestone?: any;
+  comments: number;
+  created_at: Date;
+  updated_at: Date;
+  closed_at?: any;
+  author_association: string;
+  pull_request: PullRequest;
+  body: string;
+  score: number;
+}
+
+export interface IssueSearchResult {
+  total_count: number;
+  incomplete_results: boolean;
+  items: Issue[];
 }
