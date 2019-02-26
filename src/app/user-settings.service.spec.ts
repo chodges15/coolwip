64aca12f9cdccc7f1056fb5ecc0d22e084bc6672
Chris Hodges <chris.hodges@gmail.com>
2019-02-26 09:41:45 -0600

    test: Improve test coverage of user-settings service

--- src/app/user-settings.service.spec.ts
+++ src/app/user-settings.service.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserSettingsService } from './user-settings.service';
+import { SessionLoginInformation } from './interfaces';
 
 describe('UserSettingsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: UserSettingsService;
+
+  const testSettings: SessionLoginInformation = {
+    github_api: 'test.github.com',
+    github_token: 'abcd',
+    list_of_users: 'bob, charlie'
+  };
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(UserSettingsService);
+  });
 
   it('should be created', () => {
-    const service: UserSettingsService = TestBed.get(UserSettingsService);
     expect(service).toBeTruthy();
+  });
+
+  it('should allow setting and retrieval of login information', () => {
+    service.setUserSettings(testSettings);
+    expect(service.getUserSettings() === testSettings);
   });
 });
