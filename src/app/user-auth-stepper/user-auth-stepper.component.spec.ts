47ce0188a8a2d71f3278c98f317f57cc0441335f
Chris Hodges <chris.hodges@gmail.com>
2019-04-02 21:22:32 -0500

    Stepper tests with lookup by CSS

--- src/app/user-auth-stepper/user-auth-stepper.component.spec.ts
+++ src/app/user-auth-stepper/user-auth-stepper.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatStepperModule } from '@angular/material/stepper';
-
 import { UserAuthStepperComponent } from './user-auth-stepper.component';
 import { ReactiveFormsModule} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule, MatCardModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('UserAuthStepperComponent', () => {
   let component: UserAuthStepperComponent;
@@ -32,5 +33,28 @@ describe('UserAuthStepperComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should handle settings input', () => {
+    let api: string;
+    component.onApiChange('dummyApi');
+    component.userSettingsEvent.subscribe((value) => api = value);
+
+    const stepHeaders = fixture.debugElement.queryAll(By.css('.mat-horizontal-stepper-header'));
+
+    // input the API
+    const apiInputEl = fixture.debugElement.query(By.css('mat-input-element')).nativeElement;
+
+    // select the first step
+    const stepHeaderEl = stepHeaders[3].nativeElement;
+    stepHeaderEl.click();
+    fixture.detectChanges();
+
+    // click submit
+    const submitButton = fixture.debugElement.query(By.css('mat-raised-button')).nativeElement;
+    submitButton.click();
+    fixture.detectChanges();
+
+    expect(api).toBe('dummyApi');
   });
 });
